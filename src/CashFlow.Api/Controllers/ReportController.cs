7bddaaeb3a864d8d1e6e30b962bc0a9724870b2a
joaovsousamnm <joaovsousamnm@gmail.com>
2025-03-30 17:02:17 -0300

    :tada: first commit!

---
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Mime;
+using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Application.UseCases.Expenses.Reports.Pdf;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReportController : ControllerBase
+{
+    [HttpGet("excel")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetExcel([FromServices] IGenerateExpensesReportExcelUseCase useCase, [FromHeader] DateOnly month)
+    {
+        byte[] file = await useCase.Execute(month);
+
+        if (file.Length > 0 )
+            return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
+
+        return NoContent();
+    }
+
+    [HttpGet("pdf")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> GetPdf([FromServices] IGenerateExpensesReportPdfUseCase useCase, [FromHeader] DateOnly month)
+    {
+        byte[] file = await useCase.Execute(month);
+
+        if (file.Length > 0)
+            return File(file, MediaTypeNames.Application.Pdf, "report.pdf");
+
+        return NoContent();
+    }
+}
---
+++ src/CashFlow.Api/Program.cs
@@ -0,0 +1,35 @@
+using CashFlow.Api.Filters;
+using CashFlow.Api.Middleware;
+using CashFlow.Application;
+using CashFlow.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<CultureMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
